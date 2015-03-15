cf56b8afb2cf9f8497edad4ae793232231a10755
nathangrotticelli <nathangrotticelli@gmail.com>
2015-03-15 14:33:59 -0400

    photo fix, small updates

--- contact.php
+++ contact.php
@@ -1,49 +1,9 @@
 <?php
-require_once('class.phpmailer.php');
-//include("class.smtp.php"); // optional, gets called from within class.phpmailer.php if not already loaded
-
-$mail             = new PHPMailer();
-
-$body             = '<html><h1>hi</h1></html>'
-// $body             = eregi_replace("[\]",'',$body);
-
-$mail->IsSMTP(); // telling the class to use SMTP
-$mail->Host       = "smtp.gmail.com"; // SMTP server
-// $mail->SMTPDebug  = 2;                     // enables SMTP debug information (for testing)
-                                           // 1 = errors and messages
-                                           // 2 = messages only
-$mail->SMTPAuth   = true;                  // enable SMTP authentication
-// $mail->Host       = "smtp.gmail.com"; // sets the SMTP server
-$mail->Port       = 26;                    // set the SMTP port for the GMAIL server
-$mail->Username   = "nathangrotticelli@gmail.com"; // SMTP account username
-$mail->Password   = "Housetrap123";        // SMTP account password
-
-$mail->SetFrom('nathangrotticelli@gmail.com', 'Nathan Grotticelli');
-
-$mail->AddReplyTo("nathangrotticelli@gmail.com","Nathan Grotticelli");
-
-$mail->Subject    = "PHPMailer Test Subject via smtp, basic with authentication";
-
-$mail->AltBody    = "To view the message, please use an HTML compatible email viewer!"; // optional, comment out and test
-
-$mail->MsgHTML($body);
-
-$address = "nmg2225@yahoo.com";
-$mail->AddAddress($address, "John Doe");
-
-// $mail->AddAttachment("images/bx_loader.gif");      // attachment
-// $mail->AddAttachment("images/phpmailer_mini.gif"); // attachment
-
-if(!$mail->Send()) {
-  echo "Mailer Error: " . $mail->ErrorInfo;
-} else {
-  echo "Message sent!";
-}
 
 	/* ==========================  Define variables ========================== */
 
 	#Your e-mail address
-	define("__TO__", "nathangrotticelli@gmail.com");
+	define("__TO__", "test@test.com");
 
 	#Message subject
 	define("__SUBJECT__", "");
