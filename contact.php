22297cc3513aed43deceb0f69b1d66e233449b9f
nathangrotticelli <nathangrotticelli@gmail.com>
2015-03-15 11:30:01 -0400

    product page and search bar design26

--- contact.php
+++ contact.php
@@ -1,97 +1,44 @@
 <?php
 
-	/* ==========================  Define variables ========================== */
+require_once('class.phpmailer.php');
+//include("class.smtp.php"); // optional, gets called from within class.phpmailer.php if not already loaded
 
-	#Your e-mail address
-	define("__TO__", "nathangrotticelli@gmail.com");
+$mail             = new PHPMailer();
 
-	#Message subject
-	define("__SUBJECT__", "");
+$body             = file_get_contents('about2.html');
+$body             = eregi_replace("[\]",'',$body);
 
-	#Success message
-	define('__SUCCESS_MESSAGE__', "Your message has been sent. Thank you!");
+$mail->IsSMTP(); // telling the class to use SMTP
+$mail->Host       = "smtp.gmail.com"; // SMTP server
+$mail->SMTPDebug  = 2;                     // enables SMTP debug information (for testing)
+                                           // 1 = errors and messages
+                                           // 2 = messages only
+$mail->SMTPAuth   = true;                  // enable SMTP authentication
+$mail->Host       = "smtp.gmail.com"; // sets the SMTP server
+$mail->Port       = 26;                    // set the SMTP port for the GMAIL server
+$mail->Username   = "nathangrotticelli@gmail.com"; // SMTP account username
+$mail->Password   = "Housetrap123";        // SMTP account password
 
-	#Error message
-	define('__ERROR_MESSAGE__', "Error, your message hasn't been sent");
+$mail->SetFrom('nathangrotticelli@gmail.com', 'First Last');
 
-	#Messege when one or more fields are empty
-	define('__MESSAGE_EMPTY_FILDS__', "Please fill out  all fields");
-	require_once "Mail.php";  $from = "nathan g <nathangrotticelli@gmail.com>"; $to = "nathan g22 <nmg2225@yahoo.com>"; $subject = "Hi!"; $body = "Hi,\n\nHow are you?";  $host = "mail.google.com"; $username = "nathangrotticelli@gmail.com"; $password = "Housetrap123";  $headers = array ('From' => $from,   'To' => $to,   'Subject' => $subject); $smtp = Mail::factory('smtp',   array ('host' => $host,     'auth' => true,     'username' => $username,     'password' => $password));  $mail = $smtp->send($to, $headers, $body);  if (PEAR::isError($mail)) {   echo("<p>" . $mail->getMessage() . "</p>");  } else {   echo("<p>Message successfully sent!</p>");  }
-	/* ========================  End Define variables ======================== */
+$mail->AddReplyTo("nathangrotticelli@gmail.com","First Last");
 
-	//Send mail function
-	function send_mail($to,$subject,$message,$headers){
-		if(@mail($to,$subject,$message,$headers)){
-			echo json_encode(array('info' => 'success', 'msg' => __SUCCESS_MESSAGE__));
-		} else {
-			echo json_encode(array('info' => 'error', 'msg' => __ERROR_MESSAGE__));
-		}
-	}
+$mail->Subject    = "PHPMailer Test Subject via smtp, basic with authentication";
 
-	//Check e-mail validation
-	function check_email($email){
-		if(!@eregi("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$", $email)){
-			return false;
-		} else {
-			return true;
-		}
-	}
+$mail->AltBody    = "To view the message, please use an HTML compatible email viewer!"; // optional, comment out and test
 
-	//Get post data
-	if(isset($_POST['name']) and isset($_POST['mail']) and isset($_POST['comment'])){
-		$name 	 = $_POST['name'];
-		$mail 	 = $_POST['mail'];
-		$website  = $_POST['website'];
-		$comment = $_POST['comment'];
+$mail->MsgHTML($body);
 
-		if($name == '') {
-			echo json_encode(array('info' => 'error', 'msg' => "Please enter your name."));
-			exit();
-		} else if($mail == '' or check_email($mail) == false){
-			echo json_encode(array('info' => 'error', 'msg' => "Please enter valid e-mail."));
-			exit();
-		} else if($comment == ''){
-			echo json_encode(array('info' => 'error', 'msg' => "Please enter your message."));
-			exit();
-		} else {
-			//Send Mail
-			$to = __TO__;
-			$subject = __SUBJECT__ . ' ' . $name;
-			$message = '
-			<html>
-			<head>
-			  <title>Mail from '. $name .'</title>
-			</head>
-			<body>
-			  <table style="width: 500px; font-family: arial; font-size: 14px;" border="1">
-				<tr style="height: 32px;">
-				  <th align="right" style="width:150px; padding-right:5px;">Name:</th>
-				  <td align="left" style="padding-left:5px; line-height: 20px;">'. $name .'</td>
-				</tr>
-				<tr style="height: 32px;">
-				  <th align="right" style="width:150px; padding-right:5px;">E-mail:</th>
-				  <td align="left" style="padding-left:5px; line-height: 20px;">'. $mail .'</td>
-				</tr>
-				<tr style="height: 32px;">
-				  <th align="right" style="width:150px; padding-right:5px;">subject:</th>
-				  <td align="left" style="padding-left:5px; line-height: 20px;">'. $subject .'</td>
-				</tr>
-				<tr style="height: 32px;">
-				  <th align="right" style="width:150px; padding-right:5px;">Comment:</th>
-				  <td align="left" style="padding-left:5px; line-height: 20px;">'. $comment .'</td>
-				</tr>
-			  </table>
-			</body>
-			</html>
-			';
+$address = "nmg2225@yahoo.com";
+$mail->AddAddress($address, "John Doe");
 
-			$headers  = 'MIME-Version: 1.0' . "\r\n";
-			$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-			$headers .= 'From: ' . $mail . "\r\n";
+$mail->AddAttachment("images/bx_loader.gif");      // attachment
+// $mail->AddAttachment("images/phpmailer_mini.gif"); // attachment
 
-			send_mail($to,$subject,$message,$headers);
-		}
-	} else {
-		echo json_encode(array('info' => 'error', 'msg' => __MESSAGE_EMPTY_FILDS__));
-	}
+if(!$mail->Send()) {
+  echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+  echo "Message sent!";
+}
+
  ?>
