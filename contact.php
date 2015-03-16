4db60e2716d96ffd729e843b118dba2ec464db42
nathangrotticelli <nathangrotticelli@gmail.com>
2015-03-15 21:44:58 -0400

    php mailer5

--- contact.php
+++ contact.php
@@ -1,26 +1,8 @@
 <?php
 
-require("class.phpmailer.php");
+$name = $_POST["name"];
 
-echo "Hello World";
-$mail = new PHPMailer();
+echo $name;
 
-$mail->IsSMTP();  // telling the class to use SMTP
-$mail->Host     = "smtp.gmail.com"; // SMTP server
-
-$mail->From     = "nathangrotticelli@gmail.com";
-$mail->AddAddress("nathangrotticelli@gmail.com");
-
-$mail->Subject  = "First PHPMailer Message";
-$mail->Body     = "Hi! \n\n This is my first e-mail sent through PHPMailer.";
-$mail->WordWrap = 50;
-
-if(!$mail->Send()) {
-  echo 'Message was not sent.';
-  echo 'Mailer error: ' . $mail->ErrorInfo;
-} else {
-  echo 'Message has been sent.';
-}
-echo "Hello Worl2222d";
 ?>
 
