99ff3709c929112445e612f574c9646ac8484316
nathangrotticelli <nathangrotticelli@gmail.com>
2015-03-16 00:29:41 -0400

    php mailer final2

--- js/script.js
+++ js/script.js
@@ -342,9 +342,9 @@ $(document).ready(function($) {
 
 					if(data.info !== 'error'){
 						$this.parents('form').find('input[type=text],textarea,select').filter(':visible').val('');
-						message.hide().removeClass('success').removeClass('error').addClass('success').html(data.msg).fadeIn('slow').delay(100).fadeOut('slow');
+						message.hide().removeClass('success').removeClass('error').addClass('success').html(data.msg).fadeIn('slow').delay(500).fadeOut('slow');
 					} else {
-						message.hide().removeClass('success').removeClass('error').addClass('error').html(data.msg).fadeIn('slow').delay(100).fadeOut('slow');
+						message.hide().removeClass('success').removeClass('error').addClass('error').html(data.msg).fadeIn('slow').delay(2000).fadeOut('slow');
 					}
 				}
 			});
